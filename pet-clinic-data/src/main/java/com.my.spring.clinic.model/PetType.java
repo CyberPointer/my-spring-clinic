b74c1fc8a78054cb4133f6dc9d72f3554fcb5903
bob <test@example.com>
2021-02-18 00:12:07 +0100

    Converted Person, Owners Pet and PetTypes to JPA entities. closes #37

--- pet-clinic-data/src/main/java/com.my.spring.clinic.model/PetType.java
+++ pet-clinic-data/src/main/java/com.my.spring.clinic.model/PetType.java
@@ -1,7 +1,12 @@
 package com.my.spring.clinic.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "types")
 public class PetType  extends BaseEntity{
-	
+
+	@Column(name = "name")
 	private String name;
 	
 	public String getName() {
